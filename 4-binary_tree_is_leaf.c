aacb5c97e909414edc7f7218a8fede94b6e72e4e
RockoGilbert <evolve2122@gmail.com>
2022-04-07 10:55:10 -0500

    Checks if a node is a leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,7 +8,7 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	/* check right nd left nodes */
+	/* checks right nd left nodes */
 	if (node && (!node->left && !node->right))
 		return (1);
 	return (0);
