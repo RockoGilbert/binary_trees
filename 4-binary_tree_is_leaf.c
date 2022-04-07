b29e74aa6eccb1cc8da3e41b69a1fda1eb9557fe
RockoGilbert <evolve2122@gmail.com>
2022-04-07 10:53:32 -0500

    ./4-leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - checks if node is leaf or not
+ * binary_tree_is_leaf - check if node is leaf or not
  * @node: pointer to node
  * Return: 1 if leaf, otherwise 0
  */
